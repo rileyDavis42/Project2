60b39d44bc811ce5374da717c134c655925ddf8c
Riley Davis <riguyrocks01@gmail.com>
2018-10-04 10:24:14 -0600

    Basic HTML and CSS, general JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,48 @@
+let lists = [];
+
+class List{
+    constructor(name){
+        this.name = name;
+    }
+}
+
 function add(){
-    alert("We are to add");
+    let name = prompt("What would you like to name the list?");
+    lists.push(new List(name));
+
+    //Creating the div
+    let listsDiv = document.getElementById('lists');
+    let node = document.createElement('div');
+    let listClass = document.createAttribute('class');
+    let listID = document.createAttribute('id');
+
+    //Creating the name
+    let title = document.createElement('h5');
+    title.innerHTML = name;
+
+    //adding the attributes and name
+    listClass.value = "list";
+    listID.value = "list_" + name;
+    node.setAttributeNode(listClass);
+    node.setAttributeNode(listID);
+    node.appendChild(title);
+
+    //Trash bin on the div
+    let trash = document.createElement('i');
+    let icon = document.createAttribute('class');
+    icon.value = "fas fa-trash alt";
+    let del = document.createAttribute('onclick');
+    del.value = "delet(\'list_" + name + "\')";
+    trash.setAttributeNode(icon);
+    trash.setAttributeNode(del);
+    node.appendChild(trash);
+
+    listsDiv.appendChild(node);
+}
+
+function delet(id){
+    if(confirm("Are you sure?")){
+        let element = document.getElementById(id);
+        element.parentNode.removeChild(element);
+    }
 }
